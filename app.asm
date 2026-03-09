6559dd6b469ce7ac4a2275ea31b214276b892875
Andy <100481498+PotJoke@users.noreply.github.com>
2026-03-09 11:56:25 +0300

    v0.0.7

--- app.asm
+++ app.asm
@@ -1,38 +1,71 @@
 section .data
-  text: db "TUOOT IS BETTER THAN SYHARIK", 0Ah
-  textLen: equ $-text
+	text1:	db "TUOOT IS AHEAD OF SYHARIK", 0Ah
+	text1Len: equ $-text1
+
+	text2: db "Enter number: "
+	text2Len: equ $-text2
+
+	text3: db "Your number: "
+	text3Len: equ $-text3
+
+section .bss
+	num resb 5
 
 section .text
 	global _start
-	
-  _start:
-    mov eax, 1
-    mov ecx, 8
-    
-    push eax
-    push ecx
-    
-  _loop_start:
-    mov eax, 1
-    mov edi, 1
-    lea esi, [text]
-    mov edx, textLen
-    syscall
-    
-    pop ecx
-    pop eax
-    
-    cmp eax, ecx
-    jz _end
-    
-    add eax, 1
-    
-    push eax
-    push ecx
-    
-    jmp _loop_start
-  
-  _end:
-    mov eax, 60
-    mov ebx, 0
-    syscall
+
+_print:
+	PUSH	eax
+	PUSH	ebx
+
+	MOV	eax,4
+	MOV	ebx,1
+	INT	80h
+
+	POP	ebx
+	POP	eax
+
+	RET
+
+_input:
+	PUSH	eax
+	PUSH	ebx
+	PUSH	ecx
+	PUSH	edx
+
+	MOV	eax,3
+	MOV	ebx,2
+	MOV	ecx,num
+	MOV	edx,5
+	INT	80h
+
+	POP	edx
+	POP	ecx
+	POP	ebx
+	POP	eax
+
+	RET
+
+_start:
+	MOV	ecx,text1
+	MOV	edx,text1Len
+	CALL	_print
+
+	MOV	ecx,text2
+	MOV	edx,text2Len
+	CALL	_print
+
+	CALL	_input
+
+	MOV	ecx,text3
+	MOV	edx,text3Len
+	CALL	_print
+
+	MOV	ecx,num
+	MOV	edx,5
+	CALL	_print
+
+_end:
+	MOV	eax, 1
+	MOV	ebx, 0
+	INT	80h
